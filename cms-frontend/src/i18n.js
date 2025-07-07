343e82d33b948d616444db370793288d99147dfe
Shervinhero <153194914+Shervinhero@users.noreply.github.com>
2025-07-07 17:17:30 +0200

    Update i18n.js
    synthax change

--- cms-frontend/src/i18n.js
+++ cms-frontend/src/i18n.js
@@ -12,7 +12,7 @@ i18n
             en: {
                 translation: enTranslation,
             },
-            fa: {
+            de: {
                 translation: faTranslation,
             },
         },
